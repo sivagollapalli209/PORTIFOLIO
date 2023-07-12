b9ba27880c0390a2ef6772d71feee3a3ad5525fc
Gollapalli Nagasiva durga <124057326+sivagollapalli209@users.noreply.github.com>
2023-07-12 10:13:13 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add smooth scrolling to navigation links
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
