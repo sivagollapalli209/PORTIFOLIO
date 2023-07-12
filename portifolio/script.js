a5bbdf30b07144d0b2e743f0d3c8400454c9623b
Gollapalli Nagasiva durga <124057326+sivagollapalli209@users.noreply.github.com>
2023-07-12 10:10:33 +0530

    Add files via upload

---
+++ portifolio/script.js
@@ -0,0 +1,9 @@
+// Add smooth scrolling to navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
